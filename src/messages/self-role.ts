e0e4bff4d33e60ff8ec0506b0209ebdee58be427
Gwen Phalan <gwen.phalan@gmail.com>
2022-11-12 03:53:37 -0600

    Bug Fix: Ghost Category after deleting singular role category

--- src/messages/self-role.ts
+++ src/messages/self-role.ts
@@ -4,35 +4,33 @@ import { MessageBuilder } from '../interfaces';
 import RoleCategory from '../components/buttons/role-category';
 
 export const selfRoleSettings: MessageBuilder = {
-    embeds: [new EmbedBuilder().setTitle(`Would you like to view, edit, or create a category?`)],
+    embeds: [],
     components: [],
     async build(client, guild: Guild) {
-        const categories = (await client.database.guildSettings.get(guild.id))?.selfRoles?.categories;
-        this.components = [
-            new ActionRowBuilder<ButtonBuilder>().addComponents([
-                await RoleCategory.build(client, 'view'),
-                await RoleCategory.build(client, 'edit'),
-                await RoleCategory.build(client, 'create')
-            ]),
-            new ActionRowBuilder<ButtonBuilder>().addComponents([await RoleCategory.build(client, 'message')])
-        ];
+        const categories = (await client.database.guildSettings.get(guild.id))?.selfRoles.categories;
 
-        const color = util.Color;
+        let components: ActionRowBuilder<ButtonBuilder>[] = [new ActionRowBuilder<ButtonBuilder>()];
+        const embed = new EmbedBuilder().setColor(await util.Color.getGuildColor(guild));
 
-        this.embeds[0].setColor(await color.getGuildColor(guild));
-
-        if (categories && categories.length > 0)
-            this.embeds[0].setDescription(
+        if (categories.length > 0) {
+            embed.setTitle(`Would you like to view, edit, or create a category?`).setDescription(
                 `**Categories:**\n${categories
                     .map(c => {
                         return `• ${c.name}`;
                     })
                     .join('\n')}`
             );
+            components[0].addComponents([await RoleCategory.build(client, 'view'), await RoleCategory.build(client, 'edit')]);
+            components.push(new ActionRowBuilder<ButtonBuilder>().addComponents([await RoleCategory.build(client, 'message')]));
+        } else {
+            embed.setTitle(`Click the button below to create a self-role category!`);
+        }
+
+        components[0].addComponents([await RoleCategory.build(client, 'create')]);
 
         return {
-            embeds: this.embeds,
-            components: this.components
+            embeds: [embed],
+            components: components
         };
     }
 };
