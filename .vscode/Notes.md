2aa83f4f8b79d0caffe1bfa508d25eeec2891dbe
Gwen Phalan <gwen.phalan@gmail.com>
2022-11-12 03:36:49 -0600

    // WIP

---
+++ .vscode/Notes.md
@@ -0,0 +1,10 @@
+# TODO
+
+-   Self Roles
+    -   **Back Button**
+        -   Go back to the previous page, rather just the first page of self roles
+    -   **Category Editing**
+        -   Edit the emoji
+            -   React to the message
+
+# Potential Improvements
--- src/interfaces/modal.ts
+++
@@ -1,7 +0,0 @@
-import { Collection, ModalSubmitInteraction, TextInputComponent } from 'discord.js';
-import { Client } from './Client';
-
-export type Modal = {
-    custom_id: string;
-    execute(interaction: ModalSubmitInteraction, response: Collection<string, TextInputComponent>, client: Client, label?: string): void;
-};
