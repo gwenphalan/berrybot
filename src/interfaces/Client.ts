1095b770b3811b0d7d72f1747f23d2f605582999
gwenphalan <gwen.phalan@gmail.com>
2025-04-30 22:19:55 -0700

    Removed unneeded "database" prop on client object

--- src/interfaces/Client.ts
+++ src/interfaces/Client.ts
@@ -1,6 +1,5 @@
 import { Collection, Client as BaseClient, GatewayIntentBits, Partials } from 'discord.js';
 import { config } from '../config';
-import { database } from '../database';
 import { loadComponents, loadEvents } from '../handlers';
 import type { BaseCommand, SubCommand } from './Command';
 import type { Event } from './Event';
@@ -11,7 +10,6 @@ export class Client extends BaseClient {
     subCommands = new Collection<string, SubCommand>();
     events = new Collection<string, Event['execute']>();
     messageComponents = new Collection<string, BaseMessageComponent | MessageComponent>();
-    database = database;
 
     constructor(config: { intents: GatewayIntentBits[]; partials: Partials[] }) {
         super({
