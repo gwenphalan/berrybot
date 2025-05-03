9bf334cbe0436eaf0cb6ba01fb4580aae2bcd8f1
Gwenny <gwen.phalan@proton.me>
2025-05-02 19:15:08 -0700

    f7df154 feat: reintroduce self-role management schema

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,9 +2,17 @@ import TestButton from './buttons/test';
 import TestModal from './modals/test-modal';
 import TestSelect from './selectMenus/test-select';
 import TestMultiSelect from './selectMenus/test-multi-select';
+import BackButton from './buttons/paginator/back';
+import NextButton from './buttons/paginator/next';
+import CloseButton from './buttons/paginator/close';
 
 export const buttons = {
 	TestButton,
+	Paginator: {
+		BackButton,
+		NextButton,
+		CloseButton,
+	},
 };
 
 export const modals = {
